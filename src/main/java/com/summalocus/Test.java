cabda52ef75d3183a92f2f2acc4a5bf32ad3adad
unknown <attafi-housseyn@hotmail.fr>
2023-11-08 19:26:13 +0100

    comments

--- src/main/java/com/summalocus/Test.java
+++ src/main/java/com/summalocus/Test.java
@@ -1,8 +1,9 @@
 package com.summalocus;
+// the main class
 public class Test{
 
     public static void main(String [] args){
-        
+        //methode to print string 
         System.out.println("hello word");
 
 
